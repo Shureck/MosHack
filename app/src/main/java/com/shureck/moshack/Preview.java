19a1d87f5a337b2613e78a3515e1d9304db47041
Alexander Lev <Sashalev200149@gmail.com>
2021-06-12 19:40:53 +0300

    Add markers on map

---
+++ app/src/main/java/com/shureck/moshack/Preview.java
@@ -0,0 +1,13 @@
+package com.shureck.moshack;
+
+import java.util.List;
+
+public class Preview {
+    String date;
+    String title;
+    String jpgUrl;
+    List<String> sphere;
+    Double lat;
+    Double lon;
+    String address;
+}
