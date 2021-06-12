6dbb99b37ec4a5619ef9f416c6282ea216fd3adc
Alexander Lev <Sashalev200149@gmail.com>
2021-06-12 16:11:59 +0300

    Calendar and image in Main

--- app/src/main/java/com/shureck/moshack/CalendarActivity.java
+++ app/src/main/java/com/shureck/moshack/CalendarActivity.java
@@ -1,13 +1,33 @@
 package com.shureck.moshack;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class CalendarActivity extends AppCompatActivity {
+import com.nostra13.universalimageloader.core.ImageLoader;
+
+import java.util.ArrayList;
+
+public class CalendarActivity extends AppCompatActivity implements View.OnClickListener{
+
+    public GridLayout gridLayout;
+    public ArrayList<View> surveyButtons;
+    public ArrayList<Boolean> selectedGenres;
+    public ArrayList<SurveyButtonContent> buttonContents;
+    public ArrayList<String> genresToSend;
+    String str;
+    Button button;
+    int tappedButtons;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,5 +50,57 @@ public class CalendarActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), selectedDate, Toast.LENGTH_LONG).show();
             }
         });
+
+        selectedGenres = new ArrayList<>();
+        buttonContents = new ArrayList<>();
+        genresToSend = new ArrayList<>();
+
+        buttonContents = SurveyHelper.fillSurveyContent();
+        tappedButtons = 0;
+
+        setData();
+    }
+
+    public void setData(){
+
+        button = findViewById(R.id.button);
+        gridLayout = findViewById(R.id.grid);
+
+        LayoutInflater inflater = LayoutInflater.from(gridLayout.getContext());
+
+        button.setOnClickListener(this);
+
+        surveyButtons = new ArrayList<>();
+        for (int i = 0; i < 15; i++) {
+            View newGenreButton = inflater.inflate(R.layout.element_calendar, null);
+
+            ImageView genreImage = newGenreButton.findViewById(R.id.eventImageView);
+            TextView dateTextView = newGenreButton.findViewById(R.id.dateTextView);
+            TextView sphereTextView = newGenreButton.findViewById(R.id.sphereTextView);
+            TextView freeTextView = newGenreButton.findViewById(R.id.freeTextView);
+            TextView eventHeader = newGenreButton.findViewById(R.id.eventHeader);
+
+            String sss = "";
+
+//            imageLoader.loadImage("https://www.mos.ru/upload/newsfeed/afisha/images9wlNA3(2)(15).jpg", new SimpleImageLoadingListener() {
+//                @Override
+//                public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+//                    BitmapDrawable bitmapDrawable = new BitmapDrawable(getResources(), loadedImage);
+//                    genreImage.setBackgroundDrawable(bitmapDrawable);
+//                }
+//            });
+
+            surveyButtons.add(newGenreButton.findViewById(R.id.imageButton));
+            selectedGenres.add(false);
+
+            newGenreButton.findViewById(R.id.imageButton).setOnClickListener(this);
+
+            gridLayout.addView(newGenreButton);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
