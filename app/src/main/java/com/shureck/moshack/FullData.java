4ce0f0623c4e32c18a1a752d82b3489de8bc00cf
Alexander Lev <Sashalev200149@gmail.com>
2021-06-13 10:11:44 +0300

    Greate improvement -/--

---
+++ app/src/main/java/com/shureck/moshack/FullData.java
@@ -0,0 +1,17 @@
+package com.shureck.moshack;
+
+import com.shureck.moshack.Adress;
+
+import java.util.List;
+
+public class FullData {
+    String title;
+    String text;
+    Long date_to_timestamp;
+    Long date_from_timestamp;
+    String date_from;
+    String date_to;
+    List<Adress> address;
+    String phone;
+    String jpgUrl;
+}
---
+++ app/src/main/java/com/shureck/moshack/Preview.java
@@ -0,0 +1,18 @@
+package com.shureck.moshack;
+
+import androidx.annotation.NonNull;
+
+import java.util.List;
+
+public class Preview {
+    int idItem;
+    public String date;
+    public String title;
+    String jpgUrl;
+    List<String> sphere;
+    Double lat;
+    Double lon;
+    Integer date_from_timestamp;
+    Boolean free;
+    String address;
+}
